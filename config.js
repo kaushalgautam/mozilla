4c77215645cd2ff0bb3997008751ebf3d4342884
Kaushal Gautam <swastik1706@gmail.com>
2018-03-15 06:18:16 +0000

    changed the subdirectory problem

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': "bureauveritas",
+    'database': "mongodb://tumbledore:tumbledore@ds121088.mlab.com:21088/projecttracker"
+}
---
+++ models/genre.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+    name: {type: String, required: true}
+});
+
+
+GenreSchema
+.virtual('url')
+.get(function() {
+    return '/catalog/genre/' + this._id;
+});
+
+
+module.exports = mongoose.model('Genre', GenreSchema);
